af5af5c8f967d31d787a69aca8bd73795f3a9c7a
little monster <sijanlamichhanemagar@gmail.com>
2024-10-24 18:58:29 +0530

    feat(docs) : discriptions on there working code

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,9 +1,10 @@
 export interface Point {
-  x: number;
-  y: number;
+  x: number; // The x-coordinate of the point.
+  y: number; // The y-coordinate of the point.
 }
 
+// Define the 'Drawing' interface representing a drawing with a path and color.
 export interface Drawing {
-  path: Point[];
-  color: string;
+  path: Point[]; // An array of Points that defines the path of the drawing.
+  color: string; // The color of the drawing, represented as a string (e.g., HEX, RGB).
 }
